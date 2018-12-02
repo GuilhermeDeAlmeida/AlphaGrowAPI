e61adb9084c556f2b4d48b3124497e40938617ae
Guilherme de Almeida <guilhermeaf11@hotmail.com>
2018-12-02 21:01:02 -0200

    #correcao AtuadorService para não salvar mais arquivo

--- src/main/java/com/smartgreenhouse/alphagrow/repositories/AtuadorRepository.java
+++ src/main/java/com/smartgreenhouse/alphagrow/repositories/AtuadorRepository.java
@@ -1,8 +1,11 @@
 package com.smartgreenhouse.alphagrow.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.smartgreenhouse.alphagrow.models.Atuador;
 
 public interface AtuadorRepository  extends MongoRepository<Atuador, String>{
+	List<Atuador> findByToken(String token);
 }
--- src/main/java/com/smartgreenhouse/alphagrow/services/impl/AtuadorServiceImpl.java
+++ src/main/java/com/smartgreenhouse/alphagrow/services/impl/AtuadorServiceImpl.java
@@ -2,25 +2,31 @@ package com.smartgreenhouse.alphagrow.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.smartgreenhouse.alphagrow.file.writer.AtuadorWriter;
 import com.smartgreenhouse.alphagrow.models.Atuador;
+import com.smartgreenhouse.alphagrow.repositories.AtuadorRepository;
 import com.smartgreenhouse.alphagrow.services.AtuadorService;
 
 @Service
 public class AtuadorServiceImpl implements AtuadorService{
 
+	@Autowired
+	AtuadorRepository atuadorRepository;
 	@Override
 	public List<Atuador> obterListaAtuadores(String token) {
 	
-		return AtuadorWriter.obterAtuadores(token);
+//		return AtuadorWriter.obterAtuadores(token);
+		return atuadorRepository.findByToken(token);
 	}
 
 	@Override
 	public Boolean salvarAtuador(Atuador atuador) {
-		
-		return AtuadorWriter.escreverAtuador(atuador);
+		atuadorRepository.save(atuador);
+//		return AtuadorWriter.escreverAtuador(atuador);
+		return true;
 	}
 
 }
